5054979097e21ed111d0c27c13a68e11ee958521
Md sifat <mdsifat7252@gmail.com>
2024-03-15 22:21:11 +0600

    header complete

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <a href="#Home"><img src={logo} alt="" /></a>
+            <div className="navs">
+                <li><a href="#Shop">Shop</a></li>
+                <li><a href="#Orders">Orders</a></li>
+                <li><a href="#Inventory">Inventory</a></li>
+                <li><a href="#About">About</a></li>
+                
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
